141eca3f469a6132777897576395dae410138c82
suxiao241228 <1229852569@qq.com>
2025-05-06 21:14:58 +0800

    IM-索引管理器(补充)

--- src/main/java/com/xiongsu/backend/im/BPlusTree.java
+++ src/main/java/com/xiongsu/backend/im/BPlusTree.java
@@ -16,17 +16,36 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class BPlusTree {
 
+    //作用: 这是 B+ 树与底层存储交互的接口。它负责将 Node 对象的字节数组 (raw) 存储到磁盘或内存，并根据 UID 重新加载它们。
+    // 整个 B+ 树实际上是由 DataManager 管理的一系列 DataItem（每个 DataItem 包含一个 Node 的数据）组成的。
     DataManager dm;
+    //作用: 这是这个 B+ 树索引的固定标识符。它不是根节点的UID，而是存储根节点UID的那个 DataItem 的UID。
+    // 这样做的好处是，即使根节点因为分裂而变化（UID改变），指向根节点的 bootUid 保持不变，外部只需要知道这个固定的 bootUid 就能找到这个 B+ 树的当前根。
     long bootUid;
+    //作用: 存储了 bootUid 对应的那个 DataItem 对象。这个 DataItem 的数据内容就是一个 long 值，即当前 B+ 树的根节点的 UID。
     DataItem bootDataItem;
+    //作用: 一个 ReentrantLock，用于保护 bootDataItem，确保在并发环境下对根节点 UID 的读写是线程安全的。当需要读取或更新根节点 UID 时，需要获取这个锁。
     Lock bootLock;
 
+    /**
+     * 用于在DataManager中创建一个新的空的B+树索引
+     * @param dm
+     * @return
+     * @throws Exception
+     */
     public static long create(DataManager dm) throws Exception {
         byte[] rawRoot = Node.newNilRootRaw();
         long rootUid = dm.insert(TransactionManagerImpl.SUPER_XID, rawRoot);
         return dm.insert(TransactionManagerImpl.SUPER_XID, Parser.long2Byte(rootUid));
     }
 
+    /**
+     * 用于从DataManager中加载一个已存在的B+树索引
+     * @param bootUid
+     * @param dm
+     * @return
+     * @throws Exception
+     */
     public static BPlusTree load(long bootUid, DataManager dm) throws Exception {
         DataItem bootDataItem = dm.read(bootUid);
         assert bootDataItem != null;
@@ -38,6 +57,10 @@ public class BPlusTree {
         return t;
     }
 
+    /**
+     * 获取当前B+树的根节点的UID
+     * @return
+     */
     private long rootUid() {
         bootLock.lock();
         try {
@@ -48,6 +71,13 @@ public class BPlusTree {
         }
     }
 
+    /**
+     * 用于在根节点因为分裂而产生新的根节点时，更新bootDataItem中的根节点UID
+     * @param left
+     * @param right
+     * @param rightKey
+     * @throws Exception
+     */
     private void updateRootUid(long left, long right, long rightKey) throws Exception {
         bootLock.lock();
         try {
@@ -62,6 +92,13 @@ public class BPlusTree {
         }
     }
 
+    /**
+     * 从给定的nodeUid开始，沿着树向下搜索，找到包含给定key的潜在范围的那个叶子节点
+     * @param nodeUid
+     * @param key
+     * @return
+     * @throws Exception
+     */
     private long searchLeaf(long nodeUid, long key) throws Exception {
         Node node = Node.loadNode(this, nodeUid);
         boolean isLeaf = node.isLeaf();
@@ -75,6 +112,13 @@ public class BPlusTree {
         }
     }
 
+    /**
+     * 给定一个非叶子节点的UID和一个key,找到一个继续搜索的下一个节点的UID,这个下一个节点可能是当前节点的某个子节点，也可能是当前节点的兄弟节点
+     * @param nodeUid
+     * @param key
+     * @return
+     * @throws Exception
+     */
     private long searchNext(long nodeUid, long key) throws Exception {
         while (true) {
             Node node = Node.loadNode(this, nodeUid);
@@ -85,10 +129,23 @@ public class BPlusTree {
         }
     }
 
+    /**
+     * 搜索 B+ 树中所有与精确匹配给定 key 关联的数据项 UID。
+     * @param key
+     * @return
+     * @throws Exception
+     */
     public List<Long> search(long key) throws Exception {
         return searchRange(key, key);
     }
 
+    /**
+     * 搜索 B+ 树中所有与键在 [leftKey, rightKey] 范围内的关联的数据项 UID。
+     * @param leftKey
+     * @param rightKey
+     * @return
+     * @throws Exception
+     */
     public List<Long> searchRange(long leftKey, long rightKey) throws Exception {
         long rootUid = rootUid();
         long leafUid = searchLeaf(rootUid, leftKey);
@@ -107,6 +164,12 @@ public class BPlusTree {
         return uids;
     }
 
+    /**
+     * 将一个新的键值对 (key, uid) 插入到 B+ 树中。这里的 uid 可能是指向实际数据行的指针。
+     * @param key
+     * @param uid
+     * @throws Exception
+     */
     public void insert(long key, long uid) throws Exception {
         long rootUid = rootUid();
         InsertRes res = insert(rootUid, uid, key);
@@ -116,10 +179,20 @@ public class BPlusTree {
         }
     }
 
+    //用于在插入操作中传递结果。当一个节点（无论是叶子还是非叶子）因为插入而发生分裂时，这个类用来报告分裂产生的新节点 (newNode 的 UID) 和作为分隔符的新键 (newKey)。
+    // 如果插入成功但没有分裂，或者插入导致子节点分裂但子节点的上层节点不需要分裂，newNode 通常为 0。
     class InsertRes {
         long newNode, newKey;
     }
 
+    /**
+     * 递归执行插入操作。它负责沿着树找到正确的插入路径，并在找到子节点后进行递归调用，处理子节点返回的分裂结果
+     * @param nodeUid
+     * @param uid
+     * @param key
+     * @return
+     * @throws Exception
+     */
     private InsertRes insert(long nodeUid, long uid, long key) throws Exception {
         Node node = Node.loadNode(this, nodeUid);
         boolean isLeaf = node.isLeaf();
@@ -140,6 +213,14 @@ public class BPlusTree {
         return res;
     }
 
+    /**
+     * 在给定的 nodeUid 节点中尝试插入 uid 和 key，并在必要时处理该节点的分裂。
+     * @param nodeUid
+     * @param uid
+     * @param key
+     * @return
+     * @throws Exception
+     */
     private InsertRes insertAndSplit(long nodeUid, long uid, long key) throws Exception {
         while (true) {
             Node node = Node.loadNode(this, nodeUid);
@@ -156,6 +237,9 @@ public class BPlusTree {
         }
     }
 
+    /**
+     *  释放 B+ 树所持有的资源。
+     */
     public void close() {
         bootDataItem.release();
     }
--- src/main/java/com/xiongsu/backend/im/Node.java
+++ src/main/java/com/xiongsu/backend/im/Node.java
@@ -28,6 +28,11 @@ public class Node {
     SubArray raw;
     long uid;
 
+    /**
+     * 设置是否为叶子节点，1表示是叶子节点，0表示非叶子节点
+     * @param raw
+     * @param isLeaf
+     */
     static void setRawIsLeaf(SubArray raw, boolean isLeaf) {
         if (isLeaf) {
             raw.raw[raw.start + IS_LEAF_OFFSET] = (byte)1;
@@ -36,48 +41,110 @@ public class Node {
         }
     }
 
+    /**
+     * 判断是否为叶子节点
+     * @param raw
+     * @return
+     */
     static boolean getRawIfLeaf(SubArray raw) {
         return raw.raw[raw.start + IS_LEAF_OFFSET] == (byte)1;
     }
 
+    /**
+     * 设置节点个数
+     * @param raw
+     * @param noKeys
+     */
     static void setRawNoKeys(SubArray raw, int noKeys) {
         System.arraycopy(Parser.short2Byte((short) noKeys), 0, raw.raw, raw.start+NO_KEYS_OFFSET, 2);
     }
 
+    /**
+     * 获取节点个数
+     * @param raw
+     * @return
+     */
     static int getRawNoKeys(SubArray raw) {
         return (int)Parser.parseShort(Arrays.copyOfRange(raw.raw, raw.start+SIBLING_OFFSET, raw.start+NO_KEYS_OFFSET+2));
     }
 
+    /**
+     * 设置兄弟节点的uid,占用八个字节
+     * @param raw
+     * @param sibling
+     */
     static void setRawSibling(SubArray raw, long sibling) {
         System.arraycopy(Parser.long2Byte(sibling), 0, raw.raw, raw.start+SIBLING_OFFSET, 8);
     }
 
+    /**
+     * 获取兄弟节点的uid
+     * @param raw
+     * @return
+     */
     static long getRawSibling(SubArray raw) {
         return Parser.parseLong(Arrays.copyOfRange(raw.raw, raw.start+SIBLING_OFFSET, raw.start+SIBLING_OFFSET+8));
     }
 
+    /**
+     * 设置第k个子节点的UID
+     * 注: k 是从0开始的
+     * @param raw 节点的原始字节数组
+     * @param uid 要设置的UID
+     * @param kth 子节点的索引
+     *            raw.start是字节数组的起始位置，NODE_HEADER_SIZE是节点头部的大小
+     *            kth * (8*2)是第k个子节点或键的偏移量，所以，raw.start + NODE_HEADER_SIZE + ket*(8*2)
+     *            就是第k个子节点或键再字节数组中的起始位置
+     */
     static void setRawKthSon(SubArray raw, long uid, int kth) {
         int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2);
         System.arraycopy(Parser.long2Byte(uid), 0, raw.raw, offset, 8);
     }
 
+    /**
+     * 获取第k个字节点的UID
+     * @param raw
+     * @param kth
+     * @return
+     */
     static long getRawKthSon(SubArray raw, int kth) {
         int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2);
         return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset+8));
     }
 
+    /**
+     * 设置第k个键的值
+     * @param raw
+     * @param key
+     * @param kth
+     */
     static void setRawKthKey(SubArray raw, long key, int kth) {
         int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2)+8;
         System.arraycopy(Parser.long2Byte(key), 0, raw.raw, offset, 8);
     }
 
+    /**
+     * 获取第k个键的值
+     * @param raw
+     * @param kth
+     * @return
+     */
     static long getRawKthKey(SubArray raw, int kth) {
         int offset = raw.start+NODE_HEADER_SIZE+kth*(8*2)+8;
         return Parser.parseLong(Arrays.copyOfRange(raw.raw, offset, offset+8));
     }
 
+    /**
+     * 从一个节点的原始字节数组中复制一部分数据到另一个节点的原始字节数组中
+     * @param from
+     * @param to
+     * @param kth
+     */
     static void copyRawFromKth(SubArray from, SubArray to, int kth) {
+        //计算要复制的数据再源节点的原始字节数组中的起始位置
         int offset = from.start+NODE_HEADER_SIZE+kth*(8*2);
+        //将源节点的原始字节数组中的数据复制到目标节点的原始字节数组中
+        //复制的数据包括从起始位置到源节点的原始字节数组的末尾的所有数据
         System.arraycopy(from.raw, offset, to.raw, to.start+NODE_HEADER_SIZE, from.end-offset);
     }
 
@@ -89,28 +156,56 @@ public class Node {
         }
     }
 
+
+    /*
+        [LeafFlag: 0]
+        [KeyNumber: 2]
+        [SiblingUid: 0]
+        [Son0: left][Key0: key][Son1: right][Key1: MAX_VALUE]
+
+        注：一个简单的演示
+
+                (key)
+               /     \
+              /       \
+             /         \
+          [left]     [right]
+     */
+
+
+    /**
+     * 创建一个新的根节点的原始字节数组
+     * 这个新的根节点包含两个子节点，它们的键分别是’key‘和'Long.MAX_VALUE'， UID分别是'left'和'right'
+     * @param left
+     * @param right
+     * @param key
+     * @return
+     */
     static byte[] newRootRaw(long left, long right, long key)  {
+        //创建一个新的字节数组，大小为节点的大小
         SubArray raw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
+        setRawIsLeaf(raw, false);//设置节点为非叶子节点
+        setRawNoKeys(raw, 2);//设置节点的键的数量为2
+        setRawSibling(raw, 0);//设置节点的兄弟节点的UID为0
+        setRawKthSon(raw, left, 0);//设置第0个子节点的UID为left
+        setRawKthKey(raw, key, 0);//设置第0个键的值为key
+        setRawKthSon(raw, right, 1);//设置第1个子节点的UID为right
+        setRawKthKey(raw, Long.MAX_VALUE, 1);//设置第1个键的值为Long.MAX_VALUE
 
-        setRawIsLeaf(raw, false);
-        setRawNoKeys(raw, 2);
-        setRawSibling(raw, 0);
-        setRawKthSon(raw, left, 0);
-        setRawKthKey(raw, key, 0);
-        setRawKthSon(raw, right, 1);
-        setRawKthKey(raw, Long.MAX_VALUE, 1);
-
-        return raw.raw;
+        return raw.raw;//返回新创建的根节点的原始字节数组
     }
 
+    /**
+     * 创建一个新的空根节点的原始字节数组，这个新的根节点没有子节点和键
+     * @return
+     */
     static byte[] newNilRootRaw()  {
-        SubArray raw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
+        SubArray raw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);//创建一个新的字节数组，大小为节点的大小
+        setRawIsLeaf(raw, true);//设置节点为叶子节点
+        setRawNoKeys(raw, 0);//设置节点的键的数量为0
+        setRawSibling(raw, 0);//设置节点的兄弟节点的UID为0
 
-        setRawIsLeaf(raw, true);
-        setRawNoKeys(raw, 0);
-        setRawSibling(raw, 0);
-
-        return raw.raw;
+        return raw.raw;//返回新创建的空根节点的原始字节数组
     }
 
     static Node loadNode(BPlusTree bTree, long uid) throws Exception {
@@ -142,25 +237,30 @@ public class Node {
         long siblingUid;
     }
 
+    /**
+     * 在B+树的节点中搜索下一个节点的方法
+     * @param key
+     * @return
+     */
     public SearchNextRes searchNext(long key) {
-        dataItem.rLock();
+        dataItem.rLock();//获取节点的读锁
         try {
-            SearchNextRes res = new SearchNextRes();
-            int noKeys = getRawNoKeys(raw);
+            SearchNextRes res = new SearchNextRes();// 创建一个SearchNextRes对象，用于存储搜索结果
+            int noKeys = getRawNoKeys(raw);// 获取节点个数
             for(int i = 0; i < noKeys; i ++) {
-                long ik = getRawKthKey(raw, i);
-                if(key < ik) {
-                    res.uid = getRawKthSon(raw, i);
-                    res.siblingUid = 0;
-                    return res;
+                long ik = getRawKthKey(raw, i);// 获取第i个key的值
+                if(key < ik) {// 如果key小于ik，那么找到了下一个节点
+                    res.uid = getRawKthSon(raw, i);// 设置下一个节点的UID
+                    res.siblingUid = 0;// 设置兄弟节点的UID为0
+                    return res;// 返回搜索结果
                 }
             }
-            res.uid = 0;
-            res.siblingUid = getRawSibling(raw);
-            return res;
+            res.uid = 0;// 如果没有找到下一个节点，设置uid为0
+            res.siblingUid = getRawSibling(raw); // 设置兄弟节点的UID为当前节点的兄弟节点的UID
+            return res; // 返回搜索结果
 
         } finally {
-            dataItem.rUnLock();
+            dataItem.rUnLock();//释放节点的读锁
         }
     }
 
@@ -169,20 +269,26 @@ public class Node {
         long siblingUid;
     }
 
+    /**
+     * 在B+树的叶子节点中搜索一个键值范围的办法
+     * @param leftKey
+     * @param rightKey
+     * @return
+     */
     public LeafSearchRangeRes leafSearchRange(long leftKey, long rightKey) {
-        dataItem.rLock();
+        dataItem.rLock();//获取数据项的读锁
         try {
-            int noKeys = getRawNoKeys(raw);
+            int noKeys = getRawNoKeys(raw);//获取节点中的键的数量
             int kth = 0;
-            while(kth < noKeys) {
+            while(kth < noKeys) {//找到第一个大于等于左键的键
                 long ik = getRawKthKey(raw, kth);
                 if(ik >= leftKey) {
                     break;
                 }
                 kth ++;
             }
-            List<Long> uids = new ArrayList<>();
-            while(kth < noKeys) {
+            List<Long> uids = new ArrayList<>();//创建一个列表，用于存储所有在键值范围内的子节点的UID
+            while(kth < noKeys) {//遍历所有的键，将所有小于等于右键的键对应的子节点的UID添加到列表中
                 long ik = getRawKthKey(raw, kth);
                 if(ik <= rightKey) {
                     uids.add(getRawKthSon(raw, kth));
@@ -191,16 +297,16 @@ public class Node {
                     break;
                 }
             }
-            long siblingUid = 0;
+            long siblingUid = 0;//如果所有的键都被遍历过，获取兄弟节点的UID
             if(kth == noKeys) {
                 siblingUid = getRawSibling(raw);
             }
-            LeafSearchRangeRes res = new LeafSearchRangeRes();
+            LeafSearchRangeRes res = new LeafSearchRangeRes();//创建一个LeafSearchRangeRes对象，用于存储搜索结果
             res.uids = uids;
             res.siblingUid = siblingUid;
-            return res;
+            return res;//返回搜索结果
         } finally {
-            dataItem.rUnLock();
+            dataItem.rUnLock();//释放数据项的读锁
         }
     }
 
@@ -208,44 +314,57 @@ public class Node {
         long siblingUid, newSon, newKey;
     }
 
+    /**
+     * 在B+树的节点中插入一个键值对，并在需要时分裂节点
+     * @param uid
+     * @param key
+     * @return
+     * @throws Exception
+     */
     public InsertAndSplitRes insertAndSplit(long uid, long key) throws Exception {
-        boolean success = false;
-        Exception err = null;
-        InsertAndSplitRes res = new InsertAndSplitRes();
+        boolean success = false;// 创建一个标志位，用于标记插入操作是否成功
+        Exception err = null;// 创建一个异常对象，用于存储在插入或分裂节点时发生的异常
+        InsertAndSplitRes res = new InsertAndSplitRes();// 创建一个InsertAndSplitRes对象，用于存储插入和分裂节点的结果
 
-        dataItem.before();
+        dataItem.before();// 在数据项上设置一个保存点
         try {
-            success = insert(uid, key);
-            if(!success) {
+            success = insert(uid, key); // 尝试在节点中插入键值对，并获取插入结果
+            if(!success) {// 如果插入失败，设置兄弟节点的UID，并返回结果
                 res.siblingUid = getRawSibling(raw);
                 return res;
             }
-            if(needSplit()) {
+            if(needSplit()) {// 如果需要分裂节点
                 try {
-                    SplitRes r = split();
-                    res.newSon = r.newSon;
+                    SplitRes r = split();// 分裂节点，并获取分裂结果
+                    res.newSon = r.newSon;// 设置新节点的UID和新键，并返回结果
                     res.newKey = r.newKey;
                     return res;
                 } catch(Exception e) {
-                    err = e;
+                    err = e;// 如果在分裂节点时发生错误，保存异常并抛出
                     throw e;
                 }
             } else {
-                return res;
+                return res;// 如果不需要分裂节点，直接返回结果
             }
         } finally {
-            if(err == null && success) {
+            if(err == null && success) {// 如果没有发生错误并且插入成功，提交数据项的修改
                 dataItem.after(TransactionManagerImpl.SUPER_XID);
-            } else {
+            } else { // 如果发生错误或插入失败，回滚数据项的修改
                 dataItem.unBefore();
             }
         }
     }
 
+    /**
+     * 在B+树的节点中插入一个键值对的方法
+     * @param uid
+     * @param key
+     * @return
+     */
     private boolean insert(long uid, long key) {
-        int noKeys = getRawNoKeys(raw);
-        int kth = 0;
-        while(kth < noKeys) {
+        int noKeys = getRawNoKeys(raw);// 获取节点中的键的数量
+        int kth = 0;// 初始化插入位置的索引
+        while(kth < noKeys) {// 找到第一个大于或等于要插入的键的键的位置
             long ik = getRawKthKey(raw, kth);
             if(ik < key) {
                 kth ++;
@@ -253,22 +372,22 @@ public class Node {
                 break;
             }
         }
-        if(kth == noKeys && getRawSibling(raw) != 0) return false;
+        if(kth == noKeys && getRawSibling(raw) != 0) return false;// 如果所有的键都被遍历过，并且存在兄弟节点，插入失败
 
-        if(getRawIfLeaf(raw)) {
-            shiftRawKth(raw, kth);
-            setRawKthKey(raw, key, kth);
+        if(getRawIfLeaf(raw)) {// 如果节点是叶子节点
+            shiftRawKth(raw, kth);// 在插入位置后的所有键和子节点向后移动一位
+            setRawKthKey(raw, key, kth); // 在插入位置插入新的键和子节点的UID
             setRawKthSon(raw, uid, kth);
-            setRawNoKeys(raw, noKeys+1);
-        } else {
-            long kk = getRawKthKey(raw, kth);
-            setRawKthKey(raw, key, kth);
-            shiftRawKth(raw, kth+1);
-            setRawKthKey(raw, kk, kth+1);
+            setRawNoKeys(raw, noKeys+1);// 更新节点中的键的数量
+        } else {            // 如果节点是非叶子节点
+            long kk = getRawKthKey(raw, kth);// 获取插入位置的键
+            setRawKthKey(raw, key, kth);// 在插入位置插入新的键
+            shiftRawKth(raw, kth+1);// 在插入位置后的所有键和子节点向后移动一位
+            setRawKthKey(raw, kk, kth+1);// 在插入位置的下一个位置插入原来的键和新的子节点的UID
             setRawKthSon(raw, uid, kth+1);
-            setRawNoKeys(raw, noKeys+1);
+            setRawNoKeys(raw, noKeys+1);// 更新节点中的键的数量
         }
-        return true;
+        return true;// 插入成功
     }
 
     private boolean needSplit() {
@@ -279,20 +398,27 @@ public class Node {
         long newSon, newKey;
     }
 
+    /**
+     * 分裂B+树的节点
+     * 当一个节点的键的数量达到'BALANCE_NUMBER * 2'时， 就意味着这个节点已经满了，需要进行分裂操作
+     * 分裂操作的目的是将一个满的节点分裂成两个节点，每个节点包含一半的键
+     * @return
+     * @throws Exception
+     */
     private SplitRes split() throws Exception {
-        SubArray nodeRaw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);
-        setRawIsLeaf(nodeRaw, getRawIfLeaf(raw));
-        setRawNoKeys(nodeRaw, BALANCE_NUMBER);
-        setRawSibling(nodeRaw, getRawSibling(raw));
-        copyRawFromKth(raw, nodeRaw, BALANCE_NUMBER);
-        long son = tree.dm.insert(TransactionManagerImpl.SUPER_XID, nodeRaw.raw);
-        setRawNoKeys(raw, BALANCE_NUMBER);
-        setRawSibling(raw, son);
+        SubArray nodeRaw = new SubArray(new byte[NODE_SIZE], 0, NODE_SIZE);// 创建一个新的字节数组，用于存储新节点的原始数据
+        setRawIsLeaf(nodeRaw, getRawIfLeaf(raw));// 设置新节点的叶子节点标志，与原节点相同
+        setRawNoKeys(nodeRaw, BALANCE_NUMBER); // 设置新节点的键的数量为BALANCE_NUMBER
+        setRawSibling(nodeRaw, getRawSibling(raw)); // 设置新节点的兄弟节点的UID，与原节点的兄弟节点的UID相同
+        copyRawFromKth(raw, nodeRaw, BALANCE_NUMBER);    // 从原节点的原始字节数组中复制一部分数据到新节点的原始字节数组中
+        long son = tree.dm.insert(TransactionManagerImpl.SUPER_XID, nodeRaw.raw);// 在数据管理器中插入新节点的原始数据，并获取新节点的UID
+        setRawNoKeys(raw, BALANCE_NUMBER);// 更新原节点的键的数量为BALANCE_NUMBER
+        setRawSibling(raw, son);// 更新原节点的兄弟节点的UID为新节点的UID
 
-        SplitRes res = new SplitRes();
-        res.newSon = son;
-        res.newKey = getRawKthKey(nodeRaw, 0);
-        return res;
+        SplitRes res = new SplitRes();// 创建一个SplitRes对象，用于存储分裂结果
+        res.newSon = son;// 设置新节点的UID
+        res.newKey = getRawKthKey(nodeRaw, 0);// 设置新键为新节点的第一个键的值
+        return res;// 返回分裂结果
     }
 
     @Override
